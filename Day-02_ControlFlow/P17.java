70a36aba81c356d86c8ab3620a31360a017c9632
PandeyPankaj1234 <Pankajpandey9351@gmail.com>
2025-06-18 12:35:21 +0530

    commit upto Problem 20

---
+++ Day-02_ControlFlow/P17.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class P17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the base number: ");
+        int base = sc.nextInt();
+        System.out.println("Enter the exponent: ");
+        int exponent = sc.nextInt();
+
+        long result = 1;
+        for (int i = 0; i < exponent; i++) {
+            result *= base;
+        }
+        System.out.println(base + " raised to the power of " + exponent + " is: " + result);
+    }
+}
---
+++ Day-02_ControlFlow/P19.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class P19 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter a number to find its greatest factor:");
+        int number = sc.nextInt();
+        int greatestFactor = 1; 
+        for (int i = 1; i <= number / 2; i++) { 
+            if (number % i == 0) { 
+                greatestFactor = i; 
+            }
+        }
+        System.out.println("The greatest factor of " + number + " is: " + greatestFactor);   
+    }
+}
