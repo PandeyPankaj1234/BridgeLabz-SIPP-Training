0d313d2ba078f985bc93d292e67b05f3aeeedd83
PandeyPankaj1234 <Pankajpandey9351@gmail.com>
2025-06-19 22:13:59 +0530

    Day-03 full commit

--- Day-03_Arrays/PrintTable.java
+++ Day-03_Arrays/PrintTable.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class PrintTableOfAnyNumberUsingArray {
+public class PrintTable {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number for which you want to print the multiplication table: ");
