1ce6fc420c073ade2cbee1e7ac6d769e6d14544e
PandeyPankaj1234 <Pankajpandey9351@gmail.com>
2025-06-25 13:16:46 +0530

    EXTRAS_BuiltInFunctions Problem2 of level 1  commit

---
+++ EXTRAS_BuiltInFunctions/Level1/DateArithmetic.java
@@ -0,0 +1,22 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class DateArithmetic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        System.out.print("Enter a date (yyyy-MM-dd): ");
+        String input = sc.nextLine();
+
+        LocalDate date = LocalDate.parse(input, formatter);
+
+        LocalDate updatedDate = date.plusDays(7).plusMonths(1).plusYears(2);
+
+        LocalDate finalDate = updatedDate.minusWeeks(3);
+
+        System.out.println("Final date after operations: " + finalDate.format(formatter));
+    }
+}
