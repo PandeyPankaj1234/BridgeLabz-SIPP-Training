d793e92c242279d754cbe766e48ef7cc94a7fb40
PandeyPankaj1234 <Pankajpandey9351@gmail.com>
2025-06-25 13:19:53 +0530

    EXTRAS_BuiltInFunctions Problem4 of level 1  commit

---
+++ EXTRAS_BuiltInFunctions/Level1/DateComparison.java
@@ -0,0 +1,29 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class DateComparison {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        System.out.print("Enter the first date (yyyy-MM-dd): ");
+        String input1 = sc.nextLine();
+        LocalDate date1 = LocalDate.parse(input1, formatter);
+
+        System.out.print("Enter the second date (yyyy-MM-dd): ");
+        String input2 = sc.nextLine();
+        LocalDate date2 = LocalDate.parse(input2, formatter);
+
+        if (date1.isBefore(date2)) {
+            System.out.println("The first date is before the second date.");
+        } else if (date1.isAfter(date2)) {
+            System.out.println("The first date is after the second date.");
+        } else if (date1.isEqual(date2)) {
+            System.out.println("Both dates are the same.");
+        } else {
+            System.out.println("Unexpected comparison result.");
+        }
+    }
+}
