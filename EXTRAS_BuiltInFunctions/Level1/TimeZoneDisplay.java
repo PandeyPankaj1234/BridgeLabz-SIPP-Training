c5a5ba664c2af181ac415a473c50ee7b30115ed8
PandeyPankaj1234 <Pankajpandey9351@gmail.com>
2025-06-25 13:14:06 +0530

    EXTRAS_BuiltInFunctions Problem1 of level 1  commit

---
+++ EXTRAS_BuiltInFunctions/Level1/TimeZoneDisplay.java
@@ -0,0 +1,17 @@
+import java.time.format.*;
+import java.time.*;
+
+public class TimeZoneDisplay {
+    public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
+
+        ZonedDateTime gmtTime = ZonedDateTime.now(ZoneId.of("GMT"));
+        System.out.println("Current time in GMT: " + gmtTime.format(formatter));
+
+        ZonedDateTime istTime = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+        System.out.println("Current time in IST: " + istTime.format(formatter));
+
+        ZonedDateTime pstTime = ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
+        System.out.println("Current time in PST: " + pstTime.format(formatter));
+    }
+}
